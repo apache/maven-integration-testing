ee6c80a2bc8cb9d49b2e5390b66218ccb30ea745
Christian Schulte <schulte@apache.org>
2015-12-20 19:34:40 +0100

    [MNG-5359] Declared execution in PluginMgmt gets bound to lifecycle (regression)
    o Updated to add integration test.

---
+++ core-it-suite/src/test/resources/mng-5359/src/main/java/org/apache/maven/its/mng5359/App.java
@@ -0,0 +1,13 @@
+package org.apache.maven.its.mng5359;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

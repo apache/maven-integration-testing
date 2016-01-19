e9d457d0edfe37b0d9d3fd872fe5b98b7fa167ed
Hervé Boutemy <hboutemy@apache.org>
2016-01-19 01:39:49 +0100

    [MNG-5359] updated Jira issue url

--- core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5359CleanPluginExecutionDeclarationInPluginMgmtTest.java
+++ core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5359CleanPluginExecutionDeclarationInPluginMgmtTest.java
@@ -24,7 +24,7 @@ import org.apache.maven.it.util.ResourceExtractor;
 import java.io.File;
 
 /**
- * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-5359">MNG-5359</a>.
+ * This is a test set for <a href="https://issues.apache.org/jira/browse/MNG-5359">MNG-5359</a>.
  *
  * @author Anders Hammar
  */

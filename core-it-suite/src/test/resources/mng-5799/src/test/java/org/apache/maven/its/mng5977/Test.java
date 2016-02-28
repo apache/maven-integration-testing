583bb69e005817a72e87d20f7e3bc5c61cd5e861
Christian Schulte <schulte@apache.org>
2016-02-28 05:12:18 +0100

    [MNG-5799] Incorrect execution order of plugins in the same phase

---
+++ core-it-suite/src/test/resources/mng-5799/src/test/java/org/apache/maven/its/mng5977/Test.java
@@ -0,0 +1,35 @@
+package org.apache.maven.its.mng5977;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class Test
+{
+
+    @org.junit.Test
+    public void ensureProjectPropertyHasBeenSetInTheSamePhaseBeforeTheSurefirePluginIsExecuting()
+    {
+        final String mng5799SystemProperty = System.getProperty( "mng5799SystemProperty" );
+        assertNotNull( "Expected system property.", mng5799SystemProperty );
+        assertEquals( "Expected interpolated system property.", "PROVIDED", mng5799SystemProperty );
+    }
+
+}

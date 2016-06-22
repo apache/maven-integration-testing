b440c1c5c5b190ed7f548f3e80d8c3e2d90d5d57
Christian Schulte <schulte@apache.org>
2016-06-22 17:41:42 +0200

    [MNG-5527] Dependency management import should support relocations.

--- core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
+++ core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
@@ -106,6 +106,7 @@ public class IntegrationTestSuite
         // -------------------------------------------------------------------------------------------------------------
         // suite.addTestSuite( MavenIT0108SnapshotUpdateTest.class ); -- MNG-3137
 
+        suite.addTestSuite( MavenITmng5527DependencyManagementImportRelocationsTest.class );
         suite.addTestSuite( MavenITmng4463DependencyManagementImportVersionRanges.class );
         suite.addTestSuite( MavenITmng5359CleanPluginExecutionDeclarationInPluginMgmtTest.class );
         suite.addTestSuite( MavenITmng6189SiteReportPluginsWarningTest.class );

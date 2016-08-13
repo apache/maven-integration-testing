5c9d70e01cf3e6813d9a5fa2a914e2db267eaece
Christian Schulte <schulte@apache.org>
2016-08-13 02:04:02 +0200

    [MNG-6079] 3.4 regression: cannot override version of a dependencyManagement in a submodule any more

--- core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
+++ core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
@@ -105,6 +105,7 @@ public class IntegrationTestSuite
         // -------------------------------------------------------------------------------------------------------------
         // suite.addTestSuite( MavenIT0108SnapshotUpdateTest.class ); -- MNG-3137
 
+        suite.addTestSuite( MavenITmng6079DependencyManagementImportInterpolationTest.class );
         suite.addTestSuite( MavenITmng6049VersionRangeResultFilterExtensionTest.class );
         suite.addTestSuite( MavenITmng5527DependencyManagementImportRelocationsTest.class );
         suite.addTestSuite( MavenITmng5600DependencyManagementImportExclusionsTest.class );

27b3c96a488399ccb0f6d1964b6a5a62510a6f74
Christian Schulte <schulte@apache.org>
2016-12-10 22:14:10 +0100

    [MNG-597] Imported dependencies should be available to inheritance processing
    o Updated to account for introduction of 'include' scope.

--- core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5971HierarchicalImportScopeTest.java
+++ core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5971HierarchicalImportScopeTest.java
@@ -132,6 +132,41 @@ public class MavenITmng5971HierarchicalImportScopeTest
         assertTrue( contains( dependencies4, "org.apache.maven.its.mng5971:dependency:jar:3" ) );
     }
 
+    public void testIncludeInheritanceProcessingWithProjectBasedProperties()
+        throws Exception
+    {
+        final File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-5971/include-properties" );
+
+        final Verifier verifier = newVerifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.filterFile( "../settings-template.xml", "settings.xml", "UTF-8",
+                             (Map) verifier.newDefaultFilterProperties() );
+
+        verifier.addCliOption( "-s" );
+        verifier.addCliOption( "settings.xml" );
+        verifier.executeGoals( Arrays.asList( new String[]
+        {
+            "clean", "verify"
+        } ) );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        final List<String> dependencies0 = verifier.loadLines( "target/compile.txt", "UTF-8" );
+        assertTrue( contains( dependencies0, "org.apache.maven.its.mng5971:dependency:jar:0" ) );
+
+        final List<String> dependencies1 = verifier.loadLines( "1/target/compile.txt", "UTF-8" );
+        assertTrue( contains( dependencies1, "org.apache.maven.its.mng5971:dependency:jar:1" ) );
+
+        final List<String> dependencies2 = verifier.loadLines( "1/2/target/compile.txt", "UTF-8" );
+        assertTrue( contains( dependencies2, "org.apache.maven.its.mng5971:dependency:jar:2" ) );
+
+        final List<String> dependencies3 = verifier.loadLines( "1/2/3/target/compile.txt", "UTF-8" );
+        assertTrue( contains( dependencies3, "org.apache.maven.its.mng5971:dependency:jar:3" ) );
+
+        final List<String> dependencies4 = verifier.loadLines( "1/2/3/4/target/compile.txt", "UTF-8" );
+        assertTrue( contains( dependencies4, "org.apache.maven.its.mng5971:dependency:jar:3" ) );
+    }
+
     private static boolean contains( final List<String> lines, final String pattern )
     {
         for ( int i = 0, l0 = lines.size(); i < l0; i++ )

788c695631e44de0a1d65ae8a3d52af6637a0eaa
Christian Schulte <schulte@apache.org>
2016-12-31 19:21:20 +0100

    [MNG-6139] Addition of command line option 'legacy-dependency-management'.
    This reverts commit 683e932eec9b021519824d4600f3ebf7dcbd6f28.
    There will be no such command line option following the discussion on dev@.

--- core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4720DependencyManagementExclusionMerge34Test.java
+++ core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4720DependencyManagementExclusionMerge34Test.java
@@ -67,35 +67,4 @@ public class MavenITmng4720DependencyManagementExclusionMerge34Test
         assertFalse( classpath.toString(), classpath.contains( "d-0.1.jar" ) );
     }
 
-    /**
-     * Verify the effective exclusions applied during transitive dependency resolution when both the regular
-     * dependency section and dependency management declare exclusions for a particular dependency.
-     */
-    public void testitLegacy()
-        throws Exception
-    {
-        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4720" );
-
-        Verifier verifier = newVerifier( testDir.getAbsolutePath() );
-        verifier.setAutoclean( false );
-        verifier.deleteArtifacts( "org.apache.maven.its.mng4720" );
-        verifier.addCliOption( "-s" );
-        verifier.addCliOption( "settings.xml" );
-        verifier.addCliOption( "-ldm" );
-        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", verifier.newDefaultFilterProperties() );
-        verifier.executeGoal( "validate" );
-        verifier.verifyErrorFreeLog();
-        verifier.resetStreams();
-
-        List<String> classpath = verifier.loadLines( "target/classpath.txt", "UTF-8" );
-
-        assertTrue( classpath.toString(), classpath.contains( "a-0.1.jar" ) );
-        assertTrue( classpath.toString(), classpath.contains( "c-0.1.jar" ) );
-
-        assertFalse( classpath.toString(), classpath.contains( "b-0.1.jar" ) );
-
-        // should better have been excluded as well, now it's a matter of backward-compat (-ldm)
-        assertTrue( classpath.toString(), classpath.contains( "d-0.1.jar" ) );
-    }
-
 }

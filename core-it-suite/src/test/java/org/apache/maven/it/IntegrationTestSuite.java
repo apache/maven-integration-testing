dc29b09aa2062b3b0c9d69a866cb894ed3afcc81
rfscholte <rfscholte@apache.org>
2019-11-25 08:29:27 +0100

    [MNG-6656] Support build/consumer process

--- core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
+++ core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
@@ -106,7 +106,8 @@ public class IntegrationTestSuite
         // Tests that don't run stable and need to be fixed
         // -------------------------------------------------------------------------------------------------------------
         // suite.addTestSuite( MavenIT0108SnapshotUpdateTest.class ); -- MNG-3137
-
+        
+        suite.addTestSuite( MavenITmng6656BuildConsumer.class );
         suite.addTestSuite( MavenITmng6759TransitiveDependencyRepositoriesTest.class );
         suite.addTestSuite( MavenITmng6720FailFastTest.class );
         suite.addTestSuite( MavenITmng6506PackageAnnotationTest.class );

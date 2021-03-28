9b5ca701a6c002e21eb414f3eed4ca47bec07749
Hervé Boutemy <hboutemy@apache.org>
2021-03-28 14:57:04 +0200

    simplify test and rename BOMs to bom-a and bom-b

--- core-it-suite/src/test/java/org/apache/maven/it/MavenITmng6772NestedImportScopeRepositoryOverride.java
+++ core-it-suite/src/test/java/org/apache/maven/it/MavenITmng6772NestedImportScopeRepositoryOverride.java
@@ -20,24 +20,17 @@ package org.apache.maven.it;
  */
 
 import java.io.File;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.maven.it.util.ResourceExtractor;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.endsWith;
-import static org.hamcrest.Matchers.startsWith;
 
 /**
  * This is a test set for <a href="https://issues.apache.org/jira/browse/MNG-6772">MNG-6772</a>:
  *
- * The test POM references an import scope POM, which also has a dependency on an import scope POM.
+ * The pom.xml project POM imports dependencyManagement from bom-a POM, which also imports dependencyManagement from another bom-b POM.
  *
- * Both import POMs can only be found in the repository defined in the test POM.
- * It has a parent POM that defines the same repository with a different location.
- * The test confirms that the dominant repository definition (child) wins while resolving the import POMs.
+ * Both BOM POMs can only be found in the repository defined in the project POM, that overrides central repository.
+ *
+ * The test checks that central repository override from project pom.xml remains used to get bom-b POM.
  *
  */
 public class MavenITmng6772NestedImportScopeRepositoryOverride
@@ -46,7 +39,7 @@ public class MavenITmng6772NestedImportScopeRepositoryOverride
 
     public MavenITmng6772NestedImportScopeRepositoryOverride()
     {
-        super( "[3.0,)" );
+        super( "[4.0.0-alpha-1,)" );
     }
 
     // This will test the behavior using ProjectModelResolver
@@ -59,83 +52,8 @@ public class MavenITmng6772NestedImportScopeRepositoryOverride
         overrideSettings( testDir, verifier );
         verifier.deleteArtifacts( "org.apache.maven.its.mng6772" );
 
-        verifier.filterFile( "pom-template.xml", "pom.xml", "UTF-8", verifier.newDefaultFilterProperties() );
-
-        try
-        {
-            verifier.executeGoal( "validate" );
-            fail( "Shouldn't be able to find b-0.1.pom in Central " );
-        }
-        catch( VerificationException e )
-        {
-        }
-
-        List<String> logLines = Files.readAllLines( testDir.toPath().resolve( verifier.getLogFileName() ) );
-
-        List<String> downloadLines = new ArrayList<>( 3 );
-        for ( String line : logLines )
-        {
-            if ( line.startsWith( "[INFO] Downloading from central:" ) )
-            {
-                downloadLines.add( line );
-            }
-        }
-
-        assertEquals( 2, downloadLines.size() );
-
-        assertThat( downloadLines.get( 0 ), endsWith( "/a-0.1.pom" ) );
-        assertThat( downloadLines.get( 0 ), startsWith( "[INFO] Downloading from central: file" ) );
-
-        assertThat( downloadLines.get( 1 ), endsWith( "/b-0.1.pom" ) );
-        assertThat( downloadLines.get( 1 ), startsWith( "[INFO] Downloading from central: http" ) );
-
-        verifier.resetStreams();
-    }
-
-    // This will test the behavior using DefaultModelResolver
-    public void testitInDependency()
-        throws Exception
-    {
-        final File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-6772-override-in-dependency" );
-
-        final Verifier verifier = newVerifier( testDir.getAbsolutePath(), null );
-        overrideSettings( testDir, verifier );
-        verifier.deleteArtifacts( "org.apache.maven.its.mng6772" );
-
-        verifier.filterFile( "pom-template.xml", "pom.xml", "UTF-8", verifier.newDefaultFilterProperties() );
-
-        try
-        {
-            verifier.executeGoal( "compile" );
-            fail( "Shouldn't be able to find b-0.1.pom in Central " );
-        }
-        catch( VerificationException e )
-        {
-        }
-
-        List<String> logLines = Files.readAllLines( testDir.toPath().resolve( verifier.getLogFileName() ) );
-
-        List<String> downloadLines = new ArrayList<>( 3 );
-        for ( String line : logLines )
-        {
-            if ( line.startsWith( "[INFO] Downloading from central:" ) )
-            {
-                downloadLines.add( line );
-            }
-        }
-
-        assertEquals( 3, downloadLines.size() );
-
-        assertThat( downloadLines.get( 0 ), endsWith( "/dependency-0.1.pom" ) );
-        assertThat( downloadLines.get( 0 ), startsWith( "[INFO] Downloading from central: file" ) );
-
-        // this might be a bug, shouldn't it be using the repository defined in dependency-0.1.pom, even though it is a BOM
-        assertThat( downloadLines.get( 1 ), endsWith( "/a-0.1.pom" ) );
-        assertThat( downloadLines.get( 1 ), startsWith( "[INFO] Downloading from central: file" ) );
-
-        assertThat( downloadLines.get( 2 ), endsWith( "/b-0.1.pom" ) );
-        assertThat( downloadLines.get( 2 ), startsWith( "[INFO] Downloading from central: http" ) );
-
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
         verifier.resetStreams();
     }
 

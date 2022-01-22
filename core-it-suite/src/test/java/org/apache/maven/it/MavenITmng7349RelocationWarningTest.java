77fc9086a5c244f7b953b16655cfffcdafabb1a4
Guillaume Nodet <gnodet@gmail.com>
2022-01-22 21:21:11 +0100

    Simplify test

--- core-it-suite/src/test/java/org/apache/maven/it/MavenITmng7349RelocationWarningTest.java
+++ core-it-suite/src/test/java/org/apache/maven/it/MavenITmng7349RelocationWarningTest.java
@@ -55,22 +55,16 @@ public class MavenITmng7349RelocationWarningTest
         verifier.verifyErrorFreeLog();
         List<String> lines = verifier.loadLines( verifier.getLogFileName(), "UTF-8" );
         List<String> relocated = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
         for (String line : lines) {
-            if (sb.length() > 0) {
-                sb.append("\n");
-            }
-            sb.append(line);
             if (line.contains("has been relocated")) {
                 relocated.add(line);
             }
         }
-        if (relocated.size() != 2 ) {
-            throw new VerificationException("Expected 2 relocations, but found multiple.\nBuild output:\n" + sb);
-        }
-        if (!sb.toString().contains("The artifact org.apache.maven.its.mng7349:old-dep:jar:1.0.0-SNAPSHOT has been relocated to org.apache.maven.its.mng7349:new-dep:jar:1.0.0-SNAPSHOT: Test relocation reason for old-dep")
-                || !sb.toString().contains("The artifact org.apache.maven.its.mng7349:old-plugin:jar:1.0.0-SNAPSHOT has been relocated to org.apache.maven.its.mng7349:new-plugin:jar:1.0.0-SNAPSHOT: Test relocation reason for old-plugin")) {
-            throw new VerificationException("Expected the relocation messages to be logged.\nBuild output:\n" + sb);
-        }
+        assertEquals("Expected 2 relocations, but found multiple",
+                     2, relocated.size());
+        assertTrue("Expected the relocation messages to be logged",
+                    relocated.get(0).contains("Test relocation reason for old-plugin"));
+        assertTrue("Expected the relocation messages to be logged",
+                    relocated.get(1).contains("Test relocation reason for old-dep"));
     }
 }

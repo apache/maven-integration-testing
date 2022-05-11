57b57cb24b0794ab6bcf54c9b1959835b1169d32
Tamas Cservenak <tamas@cservenak.net>
2022-05-11 10:29:35 +0200

    [MNG-7470] mvn 3.9+ IT that uses wagon (default) and native transport

---
+++ core-it-suite/src/test/java/org/apache/maven/it/MavenITmng7470ResolverTransportTest.java
@@ -0,0 +1,89 @@
+package org.apache.maven.it;
+
+import java.io.File;
+import java.util.HashMap;
+
+import org.apache.maven.it.util.ResourceExtractor;
+
+/**
+ * This is a test set for <a href="https://issues.apache.org/jira/browse/MNG-7470">MNG-7470</a>:
+ * check that Maven two bundled transports works as expected.
+ */
+public class MavenITmng7470ResolverTransportTest
+        extends AbstractMavenIntegrationTestCase
+{
+    public MavenITmng7470ResolverTransportTest()
+    {
+        super( "[3.9.0,)" );
+    }
+
+    public void testResolverTransportWagon()
+            throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-7470-resolver-transport" );
+
+        HttpServer server = HttpServer.builder()
+                .port( 0 )
+                .source( new File( testDir, "repo" ) )
+                .build();
+        server.start();
+        try
+        {
+            Verifier verifier = newVerifier( testDir.getAbsolutePath() );
+            HashMap<String, String> properties = new HashMap<>();
+            properties.put( "@port@", Integer.toString( server.port() ) );
+            verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", properties );
+
+            verifier = newVerifier( new File( testDir, "wagon" ).getAbsolutePath() );
+            verifier.deleteDirectory( "target" );
+            verifier.deleteArtifacts( "org.apache.maven.its.resolver-transport.wagon" );
+            verifier.addCliOption( "-X" );
+            verifier.addCliOption("-s" );
+            verifier.addCliOption(new File( testDir, "settings.xml" ).getAbsolutePath() );
+            verifier.addCliOption( "-Pmaven-core-it-repo" );
+            verifier.addCliOption( "-Dmaven.resolver.transport=wagon" );
+            verifier.executeGoal( "verify" );
+            verifier.verifyErrorFreeLog();
+            verifier.resetStreams();
+        }
+        finally
+        {
+            server.stop();
+        }
+    }
+
+    public void testResolverTransportNative()
+            throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-7470-resolver-transport" );
+
+        HttpServer server = HttpServer.builder()
+                .port( 0 )
+                .source( new File( testDir, "repo" ) )
+                .build();
+        server.start();
+        try
+        {
+            Verifier verifier = newVerifier( testDir.getAbsolutePath() );
+            HashMap<String, String> properties = new HashMap<>();
+            properties.put( "@port@", Integer.toString( server.port() ) );
+            verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", properties );
+
+            verifier = newVerifier( new File( testDir, "native" ).getAbsolutePath() );
+            verifier.deleteDirectory( "target" );
+            verifier.deleteArtifacts( "org.apache.maven.its.resolver-transport.native" );
+            verifier.addCliOption( "-X" );
+            verifier.addCliOption("-s" );
+            verifier.addCliOption(new File( testDir, "settings.xml" ).getAbsolutePath() );
+            verifier.addCliOption( "-Pmaven-core-it-repo" );
+            verifier.addCliOption( "-Dmaven.resolver.transport=native" );
+            verifier.executeGoal( "verify" );
+            verifier.verifyErrorFreeLog();
+            verifier.resetStreams();
+        }
+        finally
+        {
+            server.stop();
+        }
+    }
+}

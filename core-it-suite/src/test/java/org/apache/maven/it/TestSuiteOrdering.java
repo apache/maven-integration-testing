9fd68c0d577b32580cb32c925db51e3832193432
Tamas Cservenak <tamas@cservenak.net>
2024-04-24 12:49:47 +0200

    [MNG-8106] IT
    https://issues.apache.org/jira/browse/MNG-8106

--- core-it-suite/src/test/java/org/apache/maven/it/TestSuiteOrdering.java
+++ core-it-suite/src/test/java/org/apache/maven/it/TestSuiteOrdering.java
@@ -120,6 +120,10 @@ public class TestSuiteOrdering implements ClassOrderer {
          * the tests are to finishing. Newer tests are also more likely to fail, so this is
          * a fail fast technique as well.
          */
+        suite.addTestSuite(MavenITmng8106OverlappingDirectoryRolesTest.class);
+        suite.addTestSuite(MavenITmng8005IdeWorkspaceReaderUsedTest.class);
+        suite.addTestSuite(MavenITmng7967ArtifactHandlerLanguageTest.class);
+        suite.addTestSuite(MavenITmng7965PomDuplicateTagsTest.class);
         suite.addTestSuite(MavenITmng7939PluginsValidationExcludesTest.class);
         suite.addTestSuite(MavenITmng7804PluginExecutionOrderTest.class);
         suite.addTestSuite(MavenITmng7836AlternativePomSyntaxTest.class);

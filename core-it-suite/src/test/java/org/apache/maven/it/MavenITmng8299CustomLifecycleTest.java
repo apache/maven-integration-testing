7b2b3af03f1e1b4e9751808b411717c768a128a3
Guillaume Nodet <gnodet@gmail.com>
2024-10-16 00:15:22 +0200

    [MNG-8299] IT for custom lifecycle phase ordering

---
+++ core-it-suite/src/test/java/org/apache/maven/it/MavenITmng8299CustomLifecycleTest.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.maven.it;
+
+import java.io.File;
+
+import org.apache.maven.shared.verifier.Verifier;
+import org.apache.maven.shared.verifier.util.ResourceExtractor;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This is a test set for <a href="https://issues.apache.org/jira/browse/MNG-8299">MNG-8299</a>.
+ */
+class MavenITmng8299CustomLifecycleTest extends AbstractMavenIntegrationTestCase {
+
+    MavenITmng8299CustomLifecycleTest() {
+        super(ALL_MAVEN_VERSIONS);
+    }
+
+    /**
+     *  Verify that invoking the third phase will invoke the first two
+     */
+    @Test
+    void testPhaseOrdering() throws Exception {
+        File testDir = ResourceExtractor.simpleExtractResources(getClass(), "/mng-8299-custom-lifecycle");
+
+        Verifier verifier = newVerifier(new File(testDir, "CustomLifecyclePlugin").getAbsolutePath());
+        verifier.addCliArgument("install");
+        verifier.verifyErrorFreeLog();
+
+        verifier = newVerifier(new File(testDir, "CustomLifecycleProject").getAbsolutePath());
+        verifier.addCliArgument("phase3");
+        verifier.verifyErrorFreeLog();
+        verifier.verifyTextInLog("Doing Phase 1 stuff. Oh yeah baby.");
+    }
+}
